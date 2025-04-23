a6cd5462960cd620f2f5a33333919bd8d2dc142b
Prasadpodaparthi <108045723+Prasadpodaparthi@users.noreply.github.com>
2025-04-23 12:31:37 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Start Nginx server
+# Start Nginx
 nginx -g "daemon off;" &
+
+# Force metadata sync
+echo "📦 Syncing pieces..."
+node dist/packages/server/api/main.js sync-metadata &
 
 # Start backend server
 node --enable-source-maps dist/packages/server/api/main.js
